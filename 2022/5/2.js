5e8983f8baeef022a212f73344adb5ca6da68fb3
Yuriy Pankiv <pankiv97@gmail.com>
2022-12-06 00:08:09 +0200

    Added day5

---
+++ 2022/5/2.js
@@ -0,0 +1,22 @@
+// Setup
+const fs = require('fs');
+const data = fs.readFileSync('./data.txt', 'utf8');
+
+// Solution
+const [boxes, moves] = data.split('\r\n\r\n');
+
+const [countOfBoxes, ...boxStacks] = boxes.split('\r\n').reverse();
+const amountOfStacks = countOfBoxes.split(' ').filter(Number).map(Number);
+const mappedRows = amountOfStacks.map(stack =>
+  boxStacks.map(boxStackRow => boxStackRow[4 * (stack - 1) + 1]).filter(el => el !== ' ')
+)
+
+moves.split('\r\n').forEach(move => {
+  const [quantity, fromPile, toPile] = move.split(' ').filter(Number).map(Number);
+  mappedRows[toPile - 1].push(...mappedRows[fromPile - 1].splice(-quantity))
+})
+
+const result = mappedRows.map(row => row[row.length - 1]).join('');
+
+// Answer
+console.log(`-- Answer: ${result}`);
---
+++ 2022/5/README.md
@@ -0,0 +1,13 @@
+This task is mostly about data parsing. As soon as you present it in a comfortable way, the rest is simple, because the logic of "moving" boxes is really straightforward.
+
+To parse data, split taks into 2 parts - boxes and commands.
+
+Boxes are kind of an upside-down for a computer logic, so reverse rows. Now it has first row that indicates amount of piles of boxes. Extract numbers and iterate over the rest of box rows and extract letters.
+
+Commands - just extract 3 number values. Words are not needed.
+
+## Taks 1
+Add reverse to spliced array
+
+## Task 2
+No reverse needed
