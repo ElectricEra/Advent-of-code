c1d39776807c5d8406935052d93b13ba5feff1d7
Yuriy Pankiv <pankiv97@gmail.com>
2022-12-06 14:40:15 +0200

    Added task6

---
+++ 2022/6/2.js
@@ -0,0 +1,20 @@
+// Setup
+const fs = require('fs');
+const data = fs.readFileSync('./data.txt', 'utf8');
+
+// Solution 4 - 14
+for (let i = 0; i < data.length - 13; i++) {
+    const myString = data.substring(i, i + 14)
+    let hasSameLetters = false;
+    for (let j = 0; j < myString.length; j++) {
+        if (myString.indexOf(myString[j]) !== myString.lastIndexOf(myString[j])) {
+            hasSameLetters = true;
+            break
+        }
+    }
+    if (!hasSameLetters) {
+        // Answer
+        console.log(`-- Answer: ${i + 14}`);
+        break
+    }
+}
---
+++ 2022/6/README.md
@@ -0,0 +1,7 @@
+Code for taks 1 and 2 is the same, just variables are different. The logic for checking if there are duplicates is checking first and last index of a letter appearance.
+
+## Task 1
+Checking 4 letters
+
+## Taks 2
+Checking 14 letters
